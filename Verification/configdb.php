fd1f4636ef16e43e996f4ccc3157c5758bd67b58
Mary Kilewe <marykilewe@gmail.com>
2018-04-04 19:10:02 +0300

    Changed database configuration

--- Verification/configdb.php
+++ Verification/configdb.php
@@ -1,3 +1,3 @@
 <?php
-$mysqli=mysqli_connect('54.224.229.75','ubuntu','Sydney123','userreg') or die("Database Error");
+$mysqli=mysqli_connect('localhost','root','Sydney123','userreg') or die("Database Error");
 ?>
