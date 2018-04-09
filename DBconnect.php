96c06e4fcf3f88c078824535a9e54ff086d2342a
MaryKilewe <marykilewe@gmail.com>
2018-04-09 23:05:54 +0300

    Update DBconnect.php

--- DBconnect.php
+++ DBconnect.php
@@ -1,6 +1,6 @@
 <?php
   $server="localhost";
   $user="root";
-  $password="Sydney123";
+  $password="";
   $database="tastic";  
 $connection=mysqli_connect($server,$user,$password,$database) or die("NO server and database");
