1d0f8fd811a80b0d75f41e3586c76e45544592e0
Mary Kilewe <marykilewe@gmail.com>
2018-04-10 12:11:30 +0300

    loggid in user

--- auth.php
+++ auth.php
@@ -3,6 +3,6 @@ session_start();
 if(!isset($_SESSION["username"])){
 	$message = "Kindly login to perform action";
 echo "<script type='text/javascript'>alert('$message');</script>";
-header("Location: login.php");
+header("Location: indexUserLoggedIn.php");
 exit(); }
 ?>
